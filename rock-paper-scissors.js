ee81c594cb80979738e3ad71cd883f77544c9898
CalyWorld <88979648+CalyWorld@users.noreply.github.com>
2022-01-20 16:31:48 +0000

    Changed from Tradional function to arrow functions and incorporated string substitution for my return values

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,130 +1,72 @@
-function computerPlay(){
-    let randomNumber= Math.floor(Math.random() * 3);
-
-    if(randomNumber === 0){
-      return ("Rock");
-    }
-    else if(randomNumber === 1){
-      return "Paper";
-    }
-    else if(randomNumber === 2){
-      return "Scissors";
-    }
-}
-
-  let playerScore = 0;
-  let computerScore = 0;
-  let drawScore = 0;
-
-function playRound(playerSelection, computerSelection){
+  var RPS = ['rock' ,'paper' , 'scissors'];
   
-  if((playerSelection === "rock") && (computerSelection === "Rock")){
-    drawScore++;
-      return (
-        "DRAW - player chose: " +
-                    playerSelection +
-                                  " &" +
-                                     " computer chose: " +
-                            computerSelection
-            );
+  let computerPlay = (RPS)=>{
+    return RPS[Math.floor(Math.random() * RPS.length)];
   }
-  else if((playerSelection === "paper") && (computerSelection === "Paper")){
-    drawScore++;
-      return (
-        "DRAW - player chose: " +
-                    playerSelection +
-                                  " &" +
-                                     " computer chose: " +
-                            computerSelection
-            );
-  }
-  else if((playerSelection === "scissors") && (computerSelection === "Scissors")){
-    drawScore++;
-      return (
-        "DRAW - player chose: " +
-                    playerSelection +
-                                  " &" +
-                                     " computer chose: " +
-                            computerSelection
-            );
-  }
-  else if((playerSelection === "rock") && (computerSelection === "Scissors")){
-    playerScore++;
-    return (
-      "WIN - player chose: " +
-                  playerSelection +
-                                  " &" +
-                                     " computer chose: " +
-                            computerSelection
-            );
-    
-  }
-  else if((playerSelection === "paper") && (computerSelection === "Rock")){
-    playerScore++;
-    return (
-      "WIN - player chose: " +
-                  playerSelection +
-                                  " &" +
-                                     " computer chose: " +
-                            computerSelection
-            );
-  }
-  else if((playerSelection === "scissors") && (computerSelection === "Paper")){
-    playerScore++;
-    return (
-      "WIN - player chose: " +
-                  playerSelection +
-                                  " &" +
-                                     " computer chose: " +
-                            computerSelection
-            );
-  }
-  else if((playerSelection === "rock") && (computerSelection === "Paper")) {
-    computerScore++;
-    return(
-        "LOST - player chose " +
-                    playerSelection +
-                                  " &" +
-                                     " computer chose: " +
-                            computerSelection
-    );
-  }
-  else if((playerSelection === "scissors") && (computerSelection === "Rock")){
-   computerScore++;
-    return(
-        "LOST - player chose " +
-                    playerSelection +
-                                  " &" +
-                                     " computer chose: " +
-                            computerSelection
-    );
-  }
-  else if((playerSelection === "paper") && (computerSelection === "Scissors")){
-    computerScore++;
-    return(
-        "LOST - player chose " +
-                    playerSelection +
-                                  " &" +
-                                     " computer chose: " +
-                            computerSelection
-    );
-  }  
-}
-  function game(){
-   for(let i=1; i<=5; i++){
-    let playerSelection = prompt(("Enter a string").toLowerCase());
-    let computerSelection = computerPlay();
-    console.log(playRound(playerSelection, computerSelection));
-   }
-    if(playerScore===computerScore){
-     console.log("ITS A TIE, draw score: " + drawScore + " &" + " Player score: " + playerScore + " &" + " computer Score: " + computerScore);
-    }
-     else if(playerScore>computerScore){
-    console.log ("PLAYER WINS and player score: " + playerScore + " &" + " computer Score: " + computerScore + " draw Score: " + drawScore);  
+  
+    var playerScore = 0;
+    var computerScore = 0;
+    var drawScore = 0;
 
-    }else if(computerScore>playerScore){
-    console.log ("COMPUTER WINS, YOU LOSE! and computer score:  " + computerScore + " &" + " playerScore: " + playerScore + " draw Score: " + drawScore); 
-    } 
+  let playRound=(playerSelection, computerSelection)=> {
+    
+    if(playerSelection === computerSelection){
+      drawScore++;
+       return (`IT'S A TIE- player chose ${playerSelection} while computer chose ${computerSelection}
+      `);
+    }
+    else if((playerSelection === "rock") && (computerSelection === "scissors")){
+      playerScore++;
+      return (`WIN - player chose ${playerSelection} while computer chose ${computerSelection}
+      `);
+      
+    }
+    else if((playerSelection === "paper") && (computerSelection === "rock")){
+      playerScore++;
+      return (`WIN - player chose ${playerSelection} while computer chose ${computerSelection}
+      `);
+    }
+    else if((playerSelection === "scissors") && (computerSelection === "paper")){
+      playerScore++;
+      return (`WIN - player chose ${playerSelection} while computer chose ${computerSelection}
+      `);
+    }
+    else if((playerSelection === "rock") && (computerSelection === "paper")) {
+      computerScore++;
+      return(`LOST - player chose ${playerSelection} while computer chose ${computerSelection}
+      `);
+    }
+    else if((playerSelection === "scissors") && (computerSelection === "rock")){
+    computerScore++;
+    return(`LOST - player chose ${playerSelection} while computer chose ${computerSelection}
+      `);
+    }
+    else if((playerSelection === "paper") && (computerSelection === "scissors")){
+      computerScore++;
+      return(`LOST - player chose ${playerSelection} while computer chose ${computerSelection}
+      `);
+    }  
   }
-        game(); 
-   
+    let game = ()=>{
+
+    for(let i=1; i<=5; i++){
+      let playerSelection = prompt("Enter a string").toLowerCase();
+      let computerSelection = computerPlay(RPS);
+      console.log(playRound(playerSelection, computerSelection));
+    }
+
+      if(playerScore===computerScore)
+      console.log(`ITS A TIE, draw score is ${drawScore} while player score is ${playerScore} and computer score is ${computerScore}`);
+      
+
+      else if(playerScore>computerScore)
+      console.log (`PLAYER WINS and player score is ${playerScore} while computer score is ${computerScore} and draw score is ${drawScore}`);  
+
+      
+      else if(computerScore>playerScore)
+      console.log (`COMPUTER WINS, YOU LOSE! and computer score is ${computerScore} and playerScore ${playerScore} while draw Score is ${drawScore}`); 
+        
+    }
+    
+          game(); 
+    
